43d9acf555b482d32f588ba1a2b6403c91e95eea
bendeee1 <busibendi11@gmail.com>
2025-05-12 13:58:36 +0200

    :P

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import Countries from './componens/orszag';
 import './App.css';
 
 function App() {
   return (
-    <>
-    <h1>Orszagok</h1>
-    </>
+    <div>
+    <Countries/>
+    </div>
   );
 }
 
